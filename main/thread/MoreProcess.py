d812c95b2ddd372f7cefeea368906ca28c01c01a
newtonk <newtonk1993@gmail.com>
2019-06-30 21:08:12 +0800

    多线程

--- main/thread/MoreProcess.py
+++ main/thread/MoreProcess.py
@@ -1,3 +1,28 @@
 # python支持多进程
 import os
-print('Process (%s) start..' % os.getpid())
+print('Process (%s) start..' % os.getpid())  #获取父进程id
+pid = os.fork() #创建子进程 子进程和父进程都会返回一个值  父进程返回子进程id  子进程返回父进程id
+if pid == 0:
+    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+else:
+    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+
+
+print('---------------------上面运行了两个进程 会把此行以下代码运行两次-------------------------')
+# windos上没有fork调用，在windows上没法运行
+# apache就是通过fork出子进程来进行http请求
+
+# 跨平台的多进程
+from multiprocessing import Process
+import os
+
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
---
+++ main/thread/ProcessPoolDemo.py
@@ -0,0 +1,26 @@
+from multiprocessing import Pool
+import os, time, random
+
+def long_time_task(name):
+    print('Run task %s (%s)...' % (name,os.getpid()))
+    start =time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name,(end-start)))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Pool(4) #建立一个大小为4的进程池，超过就等待  默认大小cpu核心数
+    for i in range(5):
+        p.apply_async(long_time_task,args=(i,))
+    print('Waiting for all subprocess done ...')
+    p.close()# close之后就不能加新的进程了
+    p.join()
+    print('All subprocess done')
+
+
+#创建子进程
+import subprocess
+print('$ nslookup www.python.org') # 直接在命令行运行命令
+r = subprocess.call(['nslookup', 'www.python.org'])
+print('Exit code:', r)
